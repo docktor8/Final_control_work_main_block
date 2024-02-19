b990406275f448755cc26da0537e664eacba8008
Aleksey_Dokin <docktor8@mail.ru>
2024-02-19 21:11:36 +0300

    Добавлена программа

---
+++ Программа/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Console.Write("Введите размер массива: ");
+int lenght_array = int.Parse(Console.ReadLine());
+string[] array = new string[lenght_array];
+for(int i =0; i < lenght_array; i++)
+{
+    Console.Write($"Введите {i} элемент массива: ");
+    array[i] = Console.ReadLine();
+}
+int str_length_limit =3;
+int lenght_new_array =0;
+for(int i =0; i < lenght_array; i++)
+{
+    if(array[i].Length<=str_length_limit)
+    {
+        lenght_new_array++;
+    }
+}
+string[] new_array = new string[lenght_new_array];
+int count_new_array =0;
+for(int i =0; i < lenght_array; i++)
+{
+    if(array[i].Length<=str_length_limit)
+    {
+        new_array[count_new_array]=array[i];
+        Console.Write($"{new_array[count_new_array]} ");
+        count_new_array++; 
+    }    
+}
